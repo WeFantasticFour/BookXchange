c75c14c7e9a726642b5fb117d9f3da1ee60c5d26
Dharmesh Gohil <dgohil@groupon.com>
2017-10-08 17:29:49 -0700

    add fabrics support

--- app/src/main/java/com/fantastic/bookxchange/MainActivity.java
+++ app/src/main/java/com/fantastic/bookxchange/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fantastic.bookxchange;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
     }
 }
