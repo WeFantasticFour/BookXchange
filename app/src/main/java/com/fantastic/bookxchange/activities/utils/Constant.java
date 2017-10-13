303cae536d071fa9b1e399c7825c48515c3bd55f
ghilbing <ghilbing@gmail.com>
2017-10-12 17:59:27 -0700

    Starting api connection

---
+++ app/src/main/java/com/fantastic/bookxchange/activities/utils/Constant.java
@@ -0,0 +1,12 @@
+package com.fantastic.bookxchange.activities.utils;
+
+/**
+ * Created by gretel on 10/12/17.
+ */
+
+public class Constant {
+
+    public static final String BASE_URL = "http://openlibrary.org";
+
+
+}
