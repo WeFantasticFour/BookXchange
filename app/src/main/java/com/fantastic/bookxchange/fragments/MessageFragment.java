a7e8cc4377ffe1e3b989957835bade6ca4059216
ghilbing <ghilbing@gmail.com>
2017-10-14 07:27:14 -0700

    adding message fragment to review

---
+++ app/src/main/java/com/fantastic/bookxchange/fragments/MessageFragment.java
@@ -0,0 +1,123 @@
+package com.fantastic.bookxchange.fragments;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fantastic.bookxchange.R;
+import com.fantastic.bookxchange.models.User;
+
+
+public class MessageFragment extends BottomSheetDialogFragment {
+
+
+    private static final int TOTAL_CHARS = 50;
+
+    private EditText etMessage;
+    private Button btnGoToSendMessage;
+    private Button btnCancel;
+    private TextView tvCharsRemaining;
+
+    User user;
+
+
+    public MessageFragment() {
+        //Empty constructor is required for DialogFragment
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_message, container);
+
+
+    }
+
+    public static MessageFragment newInstance() {
+        MessageFragment messageFragment = new MessageFragment();
+
+        return messageFragment;
+    }
+
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        etMessage = view.findViewById(R.id.etMessage);
+        btnGoToSendMessage = view.findViewById(R.id.btnGoToSendMessage);
+        tvCharsRemaining = view.findViewById(R.id.tvCharactersRemaining);
+        btnCancel = view.findViewById(R.id.btnCancel);
+
+        getDialog().setTitle("Message");
+        // Hide soft keyboard automatically and request focus to field
+        etMessage.requestFocus();
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager) getContext().getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+        etMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                tvCharsRemaining.setText(String.valueOf(TOTAL_CHARS - s.length()) + " / " + TOTAL_CHARS);
+                if(s.length()> 0 && s.length() <= TOTAL_CHARS){
+                    btnGoToSendMessage.setEnabled(true);
+                }
+                else{
+                    btnGoToSendMessage.setEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnGoToSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO SEND MESSAGE
+            }
+        });
+
+
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        setCharsRemaining(TOTAL_CHARS);
+
+    }
+
+    private void setCharsRemaining(int totalChars) {
+        Resources res = getContext().getResources();
+        tvCharsRemaining.setText(String.format(res.getString(R.string.number), totalChars));
+
+    }
+
+
+}
---
+++ app/src/main/java/com/fantastic/bookxchange/models/Message.java
@@ -0,0 +1,83 @@
+package com.fantastic.bookxchange.models;
+
+import com.fantastic.bookxchange.utils.Utils;
+
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * Created by gretel on 10/14/17.
+ */
+
+public class Message {
+
+    private String id;
+    User recipientUser;
+    User senderUser;
+    String text;
+    String relativeDate;
+
+    public Message(){}
+
+    public User getRecipientUser() {
+        return recipientUser;
+    }
+
+    public void setRecipientUser(User recipientUser) {
+        this.recipientUser = recipientUser;
+    }
+
+    public User getSenderUser() {
+        return senderUser;
+    }
+
+    public void setSenderUser(User senderUser) {
+        this.senderUser = senderUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRelativeDate() {
+        return relativeDate;
+    }
+
+    public void setRelativeDate(String relativeDate) {
+        this.relativeDate = relativeDate;
+    }
+
+    public static Message fromJSON(JSONObject jsonObject){
+        //TODO Complete the method to get info from JSON
+        return new Message();
+    }
+
+    public static void toJSON(Message message){
+        //TODO Complete the method to send the info tho Firebase
+    }
+
+    public static String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = Utils.getTimeString(dateMillis);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+    }
+
+
+}
